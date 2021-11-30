21362b80a488c6cbf22de7da16ace747d523c488
Alessandro Oviszach <78302740+AlessandroOviszach@users.noreply.github.com>
2021-11-30 19:05:35 +0100

    add content

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,14 @@
         <ul>
             <li>Titolo: {{ title }} </li>
             <li>Titolo Originale: {{ originaltitle }} </li>
-            <li>Lingua: {{ language }} </li>
+            <li>
+                Lingua :
+                <img v-if='availableFlags.includes(language)'
+                    :src="require(`../assets/${language}.png`)"
+                    :alt="language"
+                >
+                <span v-else>{{ language }}</span>
+            </li>
             <li>Voto: {{ vote }} </li>
         </ul>
     </div>
@@ -17,11 +24,20 @@ export default {
         originaltitle : String,
         language : String,
         vote : Number,
+    },
 
+    data() {
+        return {
+            availableFlags : ['it', 'en'],
     }
+  }
 }
 </script>
 
 <style scoped lang='scss'>
-
+.card {
+    img {
+        width: 30px;
+    }
+}
 </style>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -36,7 +36,7 @@ export default {
 
   props : {
     list : Array,
-  }
+  },
 }
 </script>
 
